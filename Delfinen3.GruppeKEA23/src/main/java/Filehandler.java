5d79d13b6ed54cc423606ea75c8e6cb8e509b5c3
WalidZNaji <wana0001@stud.kea.dk>
2023-11-23 20:20:41 +0100

    9. commit fixed: Duplication of existing text in csv. Program crashing when csv is empty. Creating header if csv is empty.

--- Delfinen3.GruppeKEA23/src/main/java/Filehandler.java
+++ Delfinen3.GruppeKEA23/src/main/java/Filehandler.java
@@ -8,21 +8,18 @@ import java.util.Scanner;
 public class Filehandler {
 
     PrintStream fileWriter;
-
-    Scanner fileReader;
-   ArrayList<Medlem> medlemmereICSV = new ArrayList<>();
+    ArrayList<Medlem> medlemmereICSV = new ArrayList<>();
     File file = new File("Medlemmere.csv");
 
 
 
     public void gemTilCSV(ArrayList<Medlem> medlemmere) {
         try {
-            fileWriter = new PrintStream(new FileOutputStream(file,true));
+            fileWriter = new PrintStream(new FileOutputStream(file, true)); // Set append to true
 
             if(file.length()==0) {
                 fileWriter.println("Name,Age,MedlemID,KonkurrenceSvømmer,Aktiv");
             }
-
             for (Medlem medlem : medlemmere) {
                 String linje = medlem.getName() + "," +
                         medlem.getAge() + "," +
@@ -35,38 +32,39 @@ public class Filehandler {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-
-
     }
+
     public ArrayList<Medlem> loadMedlemmereFromCSV() {
-        try {
-            fileReader = new Scanner(file);
-            medlemmereICSV.clear();
-            ;
-            if (fileReader.hasNextLine()) {
-                fileReader.nextLine(); // Skip the header
-            }
-                while (fileReader.hasNext()) {
-                    String linje = fileReader.nextLine();
-                    String[] attributes = linje.split(",");
+        // Ny ArrayList til at gemme loaded Medlem objects
+        ArrayList<Medlem> medlemmereICSV = new ArrayList<>();
 
-                    Medlem medlem = new Medlem(attributes[0].trim(),
-                            Integer.parseInt(attributes[1].trim()),
-                            Integer.parseInt(attributes[2].trim()),
-                            Boolean.parseBoolean(attributes[3].trim()),
-                            Boolean.parseBoolean(attributes[4].trim()));
-                    medlemmereICSV.add(medlem);
-
-                } fileReader.close();
+        try (Scanner fileReader = new Scanner(file)) {
+            if (file.length() == 0) {
+                // File is empty, no need to read anything
                 return medlemmereICSV;
-
-
-
-            } catch(FileNotFoundException e){
-                throw new RuntimeException(e);
             }
+
+            // Skipping the header
+            fileReader.nextLine();
+
+            while (fileReader.hasNext()) {
+                String linje = fileReader.nextLine();
+                String[] attributes = linje.split(",");
+                Medlem medlem = new Medlem(attributes[0].trim(),
+                        Integer.parseInt(attributes[1].trim()),
+                        Integer.parseInt(attributes[2].trim()),
+                        Boolean.parseBoolean(attributes[3].trim()),
+                        Boolean.parseBoolean(attributes[4].trim()));
+                medlemmereICSV.add(medlem);
+            }
+            return medlemmereICSV;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
+
+}
+
 
 
 
